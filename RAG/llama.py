c4e6bd977209cd770ee8a9b1c660ce56876199c7
Yuezhiman <1269080767@qq.com>
2024-12-14 01:30:33 +0800

    更新llama

---
+++ RAG/llama.py
@@ -0,0 +1,61 @@
+from llama_index.graph_stores.neo4j import Neo4jGraphStore  
+username = "neo4j"   
+password = "yzm696969"         
+url = "bolt://10.222.176.204:7687"
+database = "neo4j"        
+graph_store = Neo4jGraphStore(        
+    username=username,         
+    password=password,         
+    url=url,        
+    database=database,         
+)
+from llama_index.core import StorageContext,KnowledgeGraphIndex
+from transformers import AutoTokenizer, AutoModelForCausalLM
+# from transformers import AutoTokenizer
+# tokenizer = AutoTokenizer.from_pretrained("internlm2_5-7b")
+import torch
+model_path ="/home/zhimanyue/sftpFolder/KG_project/RAG/internlm2_5-7b"
+tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+# Set `torch_dtype=torch.float16` to load model in float16, otherwise it will be loaded as float32 and might cause OOM Error.
+model = AutoModelForCausalLM.from_pretrained(model_path, torch_dtype=torch.float32, trust_remote_code=True).cuda()
+model = model.eval()
+# Settings.llm=model
+# storage_context = StorageContext.from_defaults(graph_store=graph_store)
+# index = KnowledgeGraphIndex.from_documents([], storage_context=storage_context)
+# query_engine = index.as_query_engine(    
+#     include_text=False,      
+#     response_mode="tree_summarize",       
+#     embedding_mode="hybrid",       
+#     similarity_top_k=5,       
+#     verbose=True,       
+# )         
+# response = query_engine.query("琴和芭芭拉的关系？")       
+# print(f"Response: {response}")
+"""
+from neo4j import GraphDatabase
+from llama_index import Document
+
+def get_documents_from_neo4j(graph_store):
+    # 获取数据库连接
+    driver = GraphDatabase.driver(graph_store.url, auth=(graph_store.username, graph_store.password))
+    session = driver.session(database=graph_store.database)
+    
+    # 执行查询以获取节点和关系
+    query = '
+    MATCH (p:Person)-[r:KNOWS]->(other:Person)
+    RETURN p.name AS person_name, other.name AS acquaintance_name, r.since AS since
+    '
+    
+    result = session.run(query)
+    
+    # 转换为文档格式
+    documents = []
+    for record in result:
+        # 每条记录都是一个节点和关系的实例
+        doc_text = f"{record['person_name']} knows {record['acquaintance_name']} since {record['since']}"
+        documents.append(Document(doc_text))
+    
+    session.close()
+    return documents
+documents = get_documents_from_neo4j(graph_store)
+"""
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # KG_project
 
 RelationPrompt
+
+neo4j.bat console
+
+name:neo4j
+CALL apoc.meta.data();
