2193e7cceb2f7aac7418b1170e8a54fee4b511df
Yuezhiman <1269080767@qq.com>
2024-12-15 11:37:36 +0800

    更新llama

--- RAG/llama.py
+++ RAG/llama.py
@@ -1,7 +1,7 @@
 from llama_index.graph_stores.neo4j import Neo4jGraphStore  
 username = "neo4j"   
 password = "yzm696969"         
-url = "bolt://10.222.176.204:7687"
+url = "bolt://10.230.34.43:7687"
 database = "neo4j"        
 graph_store = Neo4jGraphStore(        
     username=username,         
@@ -9,86 +9,54 @@ graph_store = Neo4jGraphStore(
     url=url,        
     database=database,         
 )
-from llama_index.core import StorageContext,KnowledgeGraphIndex
+from llama_index.core import StorageContext,KnowledgeGraphIndex,Settings
 from transformers import AutoTokenizer, AutoModelForCausalLM
 # from transformers import AutoTokenizer
 # tokenizer = AutoTokenizer.from_pretrained("internlm2_5-7b")
 import torch
-model_path ="/home/zhimanyue/sftpFolder/KG_project/RAG/internlm2_5-7b"
+model_path ="/home/zhimanyue/sftpFolder/KG_project/RAG/internlm2_5-1_8b"
 tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
 # Set `torch_dtype=torch.float16` to load model in float16, otherwise it will be loaded as float32 and might cause OOM Error.
-model = AutoModelForCausalLM.from_pretrained(model_path, torch_dtype=torch.float32, trust_remote_code=True).cuda()
-model = model.eval()
+model = AutoModelForCausalLM.from_pretrained(model_path, torch_dtype=torch.float16, trust_remote_code=True).cuda()
+from llama_index.llms.huggingface import HuggingFaceLLM
+# Wrap the Hugging Face model and tokenizer in HuggingFaceLLM
+hf_llm = HuggingFaceLLM(
+    model=model,
+    tokenizer=tokenizer,
+)
+from sentence_transformers import SentenceTransformer,models
+model="/home/zhimanyue/sftpFolder/KG_project/RAG/tao-8k"
+transformer = models.Transformer(model_name_or_path=model , max_seq_length=128)
+pooling = models.Pooling(transformer.get_word_embedding_dimension())
+storage_context = StorageContext.from_defaults(graph_store=graph_store)
+Settings.llm=hf_llm
+embed_model = SentenceTransformer(modules=[transformer, pooling])
+# Settings.embed_model = SentenceTransformer(modules=[transformer, pooling])
+# Load a local embedding model
+# model = model.eval()
 # Settings.llm=model
 # storage_context = StorageContext.from_defaults(graph_store=graph_store)
-# index = KnowledgeGraphIndex.from_documents([], storage_context=storage_context)
-# query_engine = index.as_query_engine(    
-#     include_text=False,      
-#     response_mode="tree_summarize",       
-#     embedding_mode="hybrid",       
-#     similarity_top_k=5,       
-#     verbose=True,       
-# )         
-# response = query_engine.query("琴和芭芭拉的关系？")       
-# print(f"Response: {response}")
-"""
-from neo4j import GraphDatabase
-from llama_index import Document
+# service_context = ServiceContext.from_defaults(llm=model, chunk_size=512)
 
-def get_documents_from_neo4j(graph_store):
-    # 获取数据库连接
-    driver = GraphDatabase.driver(graph_store.url, auth=(graph_store.username, graph_store.password))
-    session = driver.session(database=graph_store.database)
-    
-    # 执行查询以获取节点和关系
-    query = '
-    MATCH (p:Person)-[r:KNOWS]->(other:Person)
-    RETURN p.name AS person_name, other.name AS acquaintance_name, r.since AS since
-    '
-    
-    result = session.run(query)
-    
-    # 转换为文档格式
-    documents = []
-    for record in result:
-        # 每条记录都是一个节点和关系的实例
-        doc_text = f"{record['person_name']} knows {record['acquaintance_name']} since {record['since']}"
-        documents.append(Document(doc_text))
-    
-    session.close()
-    return documents
-documents = get_documents_from_neo4j(graph_store)
-"""from llama_index.graph_stores.neo4j import Neo4jGraphStore  
-username = "neo4j"   
-password = "yzm696969"         
-url = "bolt://10.222.201.177:7687"
-database = "neo4j"        
-graph_store = Neo4jGraphStore(        
-    username=username,         
-    password=password,         
-    url=url,        
-    database=database,         
-)
-from llama_index.core import StorageContext,KnowledgeGraphIndex
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
-model_path ="/home/zhimanyue/sftpFolder/KG_project/RAG/internlm2_5-7b"
-model = GPT2LMHeadModel.from_pretrained(model_path)
-tokenizer = GPT2Tokenizer.from_pretrained(model_path)
-
-# Settings.embed_model = OllamaEmbedding(model_name="znbang/bge:large-zh-v1.5-f32")
-# llm=
-Settings.llm=model
-storage_context = StorageContext.from_defaults(graph_store=graph_store)
-index = KnowledgeGraphIndex.from_documents([], storage_context=storage_context)
+index = KnowledgeGraphIndex.from_documents([], storage_context=storage_context,embed_model=embed_model)
 query_engine = index.as_query_engine(    
-    include_text=False,      
-    response_mode="tree_summarize",       
-    embedding_mode="hybrid",       
-    similarity_top_k=5,       
+    include_text=True,      
+    response_mode="tree_summarize",          
+    similarity_top_k=5,     
+    embedding_mode=embed_model,  
     verbose=True,       
 )         
-response = query_engine.query("琴和芭芭拉的关系？")       
+response = query_engine.query("芭芭拉的培养材料是什么")       
 print(f"Response: {response}")
+"""
+inputs = tokenizer(["琴和芭芭拉的关系"], return_tensors="pt")
+for k,v in inputs.items():
+    inputs[k] = v.cuda()
+gen_kwargs = {"max_length": 128, "top_p": 0.8, "temperature": 0.8, "do_sample": True, "repetition_penalty": 1.0}
+output = model.generate(**inputs, **gen_kwargs)
+output = tokenizer.decode(output[0].tolist(), skip_special_tokens=True)
+print(output)
+"""
 """
 from neo4j import GraphDatabase
 from llama_index import Document
